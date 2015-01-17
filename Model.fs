ae4cbd601be21c27c04a38eb4f2049db569ac42e
theSundayProgrammer <joe.mariadassou@gmail.com>
2015-01-18 09:03:02 +1100

    First Commit

---
+++ Model.fs
@@ -0,0 +1,102 @@
+﻿module JoeChakra.BrainVita
+open Microsoft.FSharp.Math
+open Microsoft.FSharp.Collections
+open System.Collections.Generic;
+let IsValid i j =
+     (i>=2 && i<=4 && j>=0 && j<7) ||
+     (j>=2 && j<=4 && i>=0 && i<7)
+ 
+let board = Array2D.init 7 7 (fun i j -> 
+            (IsValid i j) && not (i=3 && j=3) )
+ 
+let movableX i x y =
+   board.[x,y] && board.[x+i,y] && not board.[x+2*i,y]
+ 
+let movableY j x y =
+   board.[x,y] && board.[x,y+j] && not board.[x,y+2*j]
+ 
+let moveX i x y =
+    board.[x,y] <- false
+    board.[x+i,y] <- false
+    board.[x+2*i,y] <- true
+    true
+ 
+let moveXY i j x y =
+    board.[x,y] <- false
+    board.[x+i,y+j] <- false
+    board.[x+2*i,y+2*j] <- true
+    true
+ 
+let moveY j x y =
+    board.[x,y] <- false
+    board.[x,y+j] <- false
+    board.[x,y+2*j] <- true
+    true
+ 
+type Direction =
+    | East
+    | South
+    | West
+    | North
+let isMovable d x y =
+    if IsValid x y then
+        match d with
+            | East -> (IsValid (x+2) y) && (movableX 1 x y)
+            | West -> (IsValid (x-2) y) && (movableX -1 x y)
+            | South -> (IsValid x (y+2)) && (movableY 1 x y)
+            | North -> (IsValid x (y-2))  && (movableY -1 x y)
+    else
+    false
+ 
+let move d x y =
+    match d with
+        | East ->  (moveX 1 x y)
+        | West ->  (moveX -1 x y)
+        | South -> (moveY 1 x y)
+        | North -> (moveY -1 x y)
+ 
+let reverseXY i j x y =
+    board.[x,y] <- true
+    board.[x+i,y+j] <- true
+    board.[x+2*i,y+2*j] <- false
+    true
+ 
+let reverseX i x y =
+    board.[x,y] <- true
+    board.[x+i,y] <- true
+    board.[x+2*i,y] <- false
+    true
+ 
+let reverseY i x y =
+    board.[x,y] <- true
+    board.[x,y+i] <- true
+    board.[x,y+2*i] <- false
+    true
+     
+let reverse d x y =
+    match d with
+        | East -> reverseX 1 x y
+        | West -> reverseX -1 x y
+        | South -> reverseY 1 x y
+        | North -> reverseY -1 x y
+         
+let dirs = [East;South;West;North]
+ 
+let incr i j d =
+    if d<3 then
+        i, j, (d+1)
+    else if i<6 then
+        (i+1), j, 0
+    else
+        0, (j+1), 0
+         
+let rec findMove d i j =
+    if j<7 then
+        let found = isMovable (dirs.[d]) i j
+        if found then
+            true,d,i,j
+        else
+            let (i0,j0,d0) = incr i j d
+            findMove d0 i0 j0
+    else
+        false,d,i,j
---
+++ Program.fs
@@ -0,0 +1,54 @@
+﻿module JoeChakra.Main
+open Microsoft.FSharp.Math
+open Microsoft.FSharp.Collections
+open System.Collections.Generic;
+ 
+type Move= {
+   d:JoeChakra.BrainVita.Direction
+   x:int
+   y:int
+}
+ 
+let moves = new System.Collections.Generic.List<Move> ()
+//let mutable dir=North
+let mutable count=32
+let mutable found=true
+let dirs = [
+    JoeChakra.BrainVita.Direction.East;
+    JoeChakra.BrainVita.Direction.South;
+    JoeChakra.BrainVita.Direction.West;
+    JoeChakra.BrainVita.Direction.North]
+ 
+let mutable totalMoves = 0;
+let printMove X =  printfn "%d,%d %A" X.x X.y X.d
+ 
+let rec BVMove i0 j0 d0 =
+    let (found,d,i,j) = JoeChakra.BrainVita.findMove d0 i0 j0
+    if found then
+        JoeChakra.BrainVita.move dirs.[d] i j |> ignore
+        totalMoves<-totalMoves+1
+        count<-count-1
+        let move = {d=dirs.[d]; x=i; y=j}
+        moves.Add(move)
+        if count=1 then
+            moves |> Seq.iter printMove |>ignore
+            printfn ";--------total moves %d----------" totalMoves
+            true
+        else if BVMove 0 0 0 then
+            true
+        else
+            JoeChakra.BrainVita.reverse dirs.[d] i j |> ignore
+            moves.RemoveAt(moves.Count-1)
+            count<-count+1
+            let (k,l,m) = JoeChakra.BrainVita.incr i j d
+            BVMove k l m
+    else
+        false
+ 
+
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    BVMove 0 0 0  |> ignore// Learn more about F# at http://fsharp.net
+    0 // return an integer exit code
