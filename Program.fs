19e0dfb3e5f2c6c8325fe6479342c3e29c972423
theSundayProgrammer <joe.mariadassou@gmail.com>
2015-01-18 12:10:36 +1100

    Refactoring to  use FSsequences

--- Program.fs
+++ Program.fs
@@ -1,7 +1,6 @@
 ﻿module JoeChakra.Main
 open Microsoft.FSharp.Math
 open Microsoft.FSharp.Collections
-open System.Collections.Generic;
  
 type Move= {
    d:int
@@ -9,16 +8,15 @@ type Move= {
    y:int
 }
  
-let moves = new System.Collections.Generic.List<Move> ()
+
 //let mutable dir=North
 let mutable count=32
-let mutable found=true
 
  
 let mutable totalMoves = 0;
 let printMove X =  printfn "%d,%d %A" X.x X.y JoeChakra.BrainVita.dirs.[X.d]
  
-let rec BVMove mov0 =
+let rec BVMove mov0 (moves:seq<Move>) =
     let mov = JoeChakra.BrainVita.findNextMove mov0
     if mov = JoeChakra.BrainVita.invalid then
         false
@@ -28,22 +26,19 @@ let rec BVMove mov0 =
         count<-count-1
         let (i,j,d) = mov
         let move:Move = {d=d;  x=i; y=j}
-        moves.Add(move)
-        if Solve 0 then
+        if Solve (Seq.append moves [move])  then
             true
         else
             JoeChakra.BrainVita.reverse mov |> ignore
-            moves.RemoveAt(moves.Count-1)
             count<-count+1
-            let mov1 = JoeChakra.BrainVita.incr mov
-            BVMove mov1
-and Solve  k =
+            BVMove (JoeChakra.BrainVita.incr mov) moves
+and Solve  (moves:seq<Move>) =
     if count=1 then
         moves |> Seq.iter printMove |>ignore
         printfn ";--------total moves %d----------" totalMoves
         true
     else
-        BVMove JoeChakra.BrainVita.zeroMove
+        BVMove JoeChakra.BrainVita.zeroMove moves
 
  
 
@@ -51,5 +46,5 @@ and Solve  k =
 
 [<EntryPoint>]
 let main argv = 
-    Solve 0 |> ignore// Learn more about F# at http://fsharp.net
+    Solve Seq.empty |> ignore// Learn more about F# at http://fsharp.net
     0 // return an integer exit code
